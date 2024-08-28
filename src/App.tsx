d9c8d0573d5672fdee6dd61ac101712fa39cc753
Betiza Barreira <be.penques@hotmail.com>
2024-08-28 19:12:22 -0300

    definicao de rotas - react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "./styles/themes/default";
-import { GlobalStyle } from "./styles/global"
+import { GlobalStyle } from "./styles/global";
+import { Router } from "./Router";
+import { BrowserRouter } from "react-router-dom";
 
 export function App() {
 
   return (
     <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router/>
+      </BrowserRouter>
       <GlobalStyle/>
-     <h1>Hello World</h1>
+
     </ThemeProvider>
   )
 }
