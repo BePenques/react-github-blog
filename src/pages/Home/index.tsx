10ffdbc7774419651f21d0b30ca1948b3591d1d9
Betiza Barreira <be.penques@hotmail.com>
2024-08-30 19:14:30 -0300

    css section dados  user

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,32 @@
-import { Container, BoxUserInformations } from "./styles";
+import { Container, BoxUserInformations, GithubImage, GithubUserInformations,GithubUserTags } from "./styles";
+import { faGuitar  } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export function Home(){
     return (
     <Container>
-        <BoxUserInformations></BoxUserInformations>
+        <BoxUserInformations>
+            <GithubImage src={"/src/assets/avatar.png"} alt="" />
+            <GithubUserInformations>
+                <h3>Cameron Williamson</h3>
+                <p>Tristique volutpat pulvinar vel massa, pellentesque egestas. Eu viverra massa quam <br />
+                     dignissim aenean malesuada suscipit. Nunc, volutpat pulvinar vel mass.</p>
+                <GithubUserTags>
+                  <span>
+                    <FontAwesomeIcon icon={faGuitar} />
+                    <p>cameronwll</p>
+                  </span>
+                  <span>
+                    <FontAwesomeIcon icon={faGuitar} />
+                    <p>cameronwll</p>
+                  </span>
+                  <span>
+                    <FontAwesomeIcon icon={faGuitar} />
+                    <p>cameronwll</p>
+                  </span>
+                </GithubUserTags>
+            </GithubUserInformations>
+        </BoxUserInformations>
     </Container>
     )
 
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -7,8 +7,55 @@ export const Container = styled.div`
     padding: 0 1.5rem;
 `;
 
-export const BoxUserInformations = styled.div`
-     width: 100%;
-     height: 212px;
-     /* background-color: red; */
+export const BoxUserInformations = styled.section`
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 212px;
+     
+    background: ${props => props.theme["base-profile"]};
+ 
+    border-radius: 10px;
+
+    margin-top: -5rem;
+    gap: 2rem;
 `
+
+export const GithubUserInformations = styled.div`
+    height: 148px;
+    width: 630px;
+    margin: auto 0;
+
+    h3{
+        font-size: 24px;
+    }
+    p{
+        font-size: 16px;
+        line-height: 160%;
+        margin-bottom: 24px;
+    }
+`
+
+export const GithubUserTags = styled.div`
+   display: flex;
+   flex-direction: row;
+   gap: 24px;
+
+   span{
+    display: flex;
+    flex-direction: row;
+    gap: 4px;
+    
+   }
+`
+
+export const GithubImage = styled.img.attrs((props) => ({
+    src: props.src,
+}))`
+    width: 148px;
+    height: 148px;
+    border-radius: 8px;
+`
+
