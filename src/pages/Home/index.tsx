3722895fbb49ee0268b0eb9c1aba89af713228ba
Betiza Barreira <be.penques@hotmail.com>
2024-08-31 13:51:00 -0300

    stilização home - dados user e input de busca

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,6 @@
-import { Container, BoxUserInformations, GithubImage, GithubUserInformations,GithubUserTags } from "./styles";
-import { faGuitar  } from '@fortawesome/free-solid-svg-icons';
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { Container, BoxUserInformations, GithubImage, GithubUserInformations,GithubUserTags, GithubLink, SearchBox, SearchInput, IssuesList, Issue } from "./styles";
+import {  faArrowUpRightFromSquare, faBuilding, faUserGroup  } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export function Home(){
@@ -8,25 +9,50 @@ export function Home(){
         <BoxUserInformations>
             <GithubImage src={"/src/assets/avatar.png"} alt="" />
             <GithubUserInformations>
-                <h3>Cameron Williamson</h3>
-                <p>Tristique volutpat pulvinar vel massa, pellentesque egestas. Eu viverra massa quam <br />
-                     dignissim aenean malesuada suscipit. Nunc, volutpat pulvinar vel mass.</p>
-                <GithubUserTags>
-                  <span>
-                    <FontAwesomeIcon icon={faGuitar} />
-                    <p>cameronwll</p>
-                  </span>
-                  <span>
-                    <FontAwesomeIcon icon={faGuitar} />
-                    <p>cameronwll</p>
-                  </span>
-                  <span>
-                    <FontAwesomeIcon icon={faGuitar} />
-                    <p>cameronwll</p>
-                  </span>
-                </GithubUserTags>
+              <GithubLink>
+                <p>GITHUB</p>
+                <FontAwesomeIcon icon={faArrowUpRightFromSquare} fontSize="12"/>
+              </GithubLink>        
+              <h3>Cameron Williamson</h3>
+              <p>Tristique volutpat pulvinar vel massa, pellentesque egestas. Eu viverra massa quam
+                    dignissim aenean malesuada suscipit. Nunc, volutpat pulvinar vel mass.</p>
+              <GithubUserTags>
+                <span>
+                  <FontAwesomeIcon icon={faGithub} color="#3A536B" fontSize={18}/>
+                  <p>cameronwll</p>
+                </span>
+                <span>
+                  <FontAwesomeIcon icon={faBuilding} color="#3A536B" fontSize={18}/>
+                  <p>Rocketseat</p>
+                </span>
+                <span>
+                  <FontAwesomeIcon icon={faUserGroup} color="#3A536B" fontSize={18}/>
+                  <p>32 seguidores</p>
+                </span>
+              </GithubUserTags>
             </GithubUserInformations>
         </BoxUserInformations>
+        <SearchBox>
+          <div>
+            <h3>Publicações</h3>
+            <p>6 Publicações</p>
+          </div>
+          <SearchInput type="text" placeholder="Buscar conteúdo"/>
+        </SearchBox>
+        <IssuesList>
+          <Issue>  
+            <h3>JavaScript data types and data structures</h3>
+            <p>
+            Programming languages all have built-in data structures, but these often differ from one language to another. This article attempts to list the built-in data structures available in JavaScript and what properties they have. These can be used to build other data structures. Wherever possible, comparisons with other languages are drawn. Dynamic typing JavaScript is a loosely typed and dynamic language. Variables in JavaScript are not directly associated with any particular value type, and any variable can be assigned (and re-assigned) values of all types: let foo = 42; // foo is now a number foo = 'bar'; // foo is now a string foo = true; // foo is now a boolean  
+            </p>     
+          </Issue>
+          <Issue>         
+          </Issue>
+          <Issue>         
+          </Issue>
+          <Issue>         
+          </Issue>
+        </IssuesList>
     </Container>
     )
 
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -25,15 +25,16 @@ export const BoxUserInformations = styled.section`
 
 export const GithubUserInformations = styled.div`
     height: 148px;
-    width: 630px;
-    margin: auto 0;
+    /* width: 600px; */
+    padding-top: 5px;
+    margin-right: 32px;
 
     h3{
         font-size: 24px;
+         margin-bottom: 8px;
     }
     p{
         font-size: 16px;
-        line-height: 160%;
         margin-bottom: 24px;
     }
 `
@@ -41,15 +42,81 @@ export const GithubUserInformations = styled.div`
 export const GithubUserTags = styled.div`
    display: flex;
    flex-direction: row;
+   align-items: center;
    gap: 24px;
+  
 
    span{
     display: flex;
     flex-direction: row;
-    gap: 4px;
     
+    gap: 4px;
+  
    }
 `
+
+export const GithubLink = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: baseline;
+    gap: 8px;
+    float: right ;
+    margin-right: 32px;
+    font-size: 12px;
+
+`
+//--------search form
+export const SearchBox = styled.div`
+   display: flex;
+   flex-direction: column;
+   margin-top: 72px;
+   gap: 12px;
+   div{
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    
+    h3{
+        font-size: 18px;
+    }
+    p{
+        font-size: 14px;
+    }
+   }
+`
+export const SearchInput = styled.input`
+    height: 50px;
+    border-radius: 6px;
+    background: ${props => props.theme["base-input"]};
+    border: 1px solid ${props => props.theme["base-border"]};
+    padding: 16px 12px;
+    color:  ${props => props.theme["base-text"]};
+    
+    &::placeholder{
+        color:  ${props => props.theme["base-label"]};
+    }
+`
+//-----IssuesList
+export const IssuesList = styled.div`
+    margin-top: 16px;
+    margin-bottom: 16px;
+    display: flex;
+    flex-direction: row;
+    height: auto;
+    flex-wrap: wrap;
+    gap: 2rem;
+    
+`
+export const Issue = styled.div`
+   /* width: 416px; */
+   width: 390px;
+   height: 260px;
+   background: ${props => props.theme["base-post"]}; 
+   border: 1px solid red;
+   margin-top: 32px;
+    
+`
+
 
 export const GithubImage = styled.img.attrs((props) => ({
     src: props.src,
@@ -57,5 +124,10 @@ export const GithubImage = styled.img.attrs((props) => ({
     width: 148px;
     height: 148px;
     border-radius: 8px;
+    margin-left: 40px;
+    vertical-align: middle;
 `
+
+
+
 
